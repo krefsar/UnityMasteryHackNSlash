b7a3855b2632954a39f0f129f8f4b44b3e78e5de
Evan Martinez <evanm.design@gmail.com>
2020-05-24 15:14:33 -0400

    track multiple characters

---
+++ Assets/Scripts/PlayerTracker.cs
@@ -0,0 +1,24 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+public class PlayerTracker : MonoBehaviour
+{
+    private CinemachineTargetGroup targetGroup;
+
+    private void Awake()
+    {
+        targetGroup = GetComponent<CinemachineTargetGroup>();
+
+        var players = FindObjectsOfType<Player>();
+        foreach (var player in players)
+        {
+            player.OnCharacterChanged += (character) => HandlePlayerCharacterChanged(player, character);
+        }
+    }
+
+    private void HandlePlayerCharacterChanged(Player player, Character character)
+    {
+        int playerIndex = player.PlayerNumber - 1;
+        targetGroup.m_Targets[playerIndex].target = character.transform;
+    }
+}
