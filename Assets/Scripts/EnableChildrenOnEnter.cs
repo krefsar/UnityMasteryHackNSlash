ade71a2d94d57397ea6779c4e5eacace1e92c487
Evan Martinez <evanm.design@gmail.com>
2020-05-25 15:58:43 -0400

    add enemy spawner area trigger

---
+++ Assets/Scripts/EnableChildrenOnEnter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableChildrenOnEnter : MonoBehaviour
+{
+    [SerializeField]
+    private float radius = 15f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Character>() != null)
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                transform.GetChild(i).gameObject.SetActive(true);
+            }
+        }
+    }
+
+    private void OnValidate()
+    {
+        var collider = GetComponent<Collider>();
+        if (collider == null)
+        {
+            collider = gameObject.AddComponent<SphereCollider>();
+            ((SphereCollider)collider).radius = radius;
+        }
+
+        collider.isTrigger = true;
+    }
+}
