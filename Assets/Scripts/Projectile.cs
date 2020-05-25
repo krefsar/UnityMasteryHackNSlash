10b45ed511686a43ca5bb82016c507bc26fafee7
Evan Martinez <evanm.design@gmail.com>
2020-05-25 12:16:50 -0400

    wizard shoots fireballs

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Projectile : PooledMonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 10f;
+
+    private void Update()
+    {
+        transform.position += transform.forward * moveSpeed * Time.deltaTime;
+    }
+}
--- Assets/Scripts/ProjectileAttacker.cs
+++ Assets/Scripts/ProjectileAttacker.cs
@@ -10,7 +10,7 @@ public class ProjectileAttacker : MonoBehaviour, IAttack
     [SerializeField]
     private float attackRefreshSpeed = 1f;
     [SerializeField]
-    private GameObject projectilePrefab;
+    private Projectile projectilePrefab;
 
     private Animator animator;
 
@@ -32,6 +32,6 @@ public class ProjectileAttacker : MonoBehaviour, IAttack
 
         animator.SetTrigger("Attack");
 
-        var projectile = Instantiate(projectilePrefab, transform.position, transform.rotation);
+        projectilePrefab.Get<PooledMonoBehaviour>(transform.position, transform.rotation);
     }
 }
